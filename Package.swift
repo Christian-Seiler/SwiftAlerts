44addb97c7bfa479b34332856f545669b27c9996
Christian Seiler <Christian-Seiler@users.noreply.github.com>
2019-12-08 19:40:18 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "Swift-Alerts",
+    name: "SwiftAlerts",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Swift-Alerts",
+            name: "SwiftAlerts",
             targets: ["Alerts"])
     ],
     dependencies: [
