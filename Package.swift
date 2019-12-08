77edacbd90f0f60c2bf8de5816889e3f4bafc917
Christian Seiler <christian@christianseiler.ch>
2019-12-08 19:31:28 +0100

    update package

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Swift-Components",
+    name: "Swift-Alerts",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Swift-Components",
-            targets: ["Components"])
+            name: "Swift-Alerts",
+            targets: ["Alerts"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,11 +21,11 @@ let package = Package(
         // Targets can depend on other targets in this package,
         // and on products in packages which this package depends on.
         .target(
-            name: "Components",
+            name: "Alerts",
             dependencies: []),
         .testTarget(
-            name: "ComponentsTests",
-            dependencies: ["Components"])
+            name: "AlertsTests",
+            dependencies: ["Alerts"])
     ],
     swiftLanguageVersions: [.v5]
 )
