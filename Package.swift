aa7341c372ef90c09ff8e96ab9871a8d26112b4a
Christian Seiler <christian@christianseiler.ch>
2019-12-09 07:17:43 +0100

    renaming

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftAlerts",
-            targets: ["Alerts"])
+            targets: ["SwiftAlerts"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,11 +24,11 @@ let package = Package(
         // Targets can depend on other targets in this package,
         // and on products in packages which this package depends on.
         .target(
-            name: "Alerts",
+            name: "SwiftAlerts",
             dependencies: []),
         .testTarget(
-            name: "AlertsTests",
-            dependencies: ["Alerts"])
+            name: "SwiftAlertsTests",
+            dependencies: ["SwiftAlerts"])
     ],
     swiftLanguageVersions: [.v5]
 )
